a0518084eb7b17269b5bf67fd8002cce557cf8c7
Stefan Kuznetsov <neosloth@posteo.net>
2023-04-13 00:16:32 -0700

    Mypy fixes

--- hunting_hawk/mediawiki/filepath.py
+++ hunting_hawk/mediawiki/filepath.py
@@ -1,3 +1,4 @@
+import requests
 from .client import Client, ClientError, raw_cached_get
 
 ImageName = str
@@ -6,8 +7,9 @@ ImageName = str
 def get_file_path(client: Client, file: ImageName) -> str:
     url = f"{client.index_endpoint()}/Special:FilePath/{file}"
     try:
-        with requests.get(url, follow_redirects=True, headers=client.headers, stream=True) as res:
-            url = res.url
+        res = requests.get(url, allow_redirects=True, headers=client.headers, stream=True)
+        res.close()
+        url = res.url
         return url
-    except requests.exception.HTTPError as e:
+    except requests.exceptions.HTTPError as e:
         raise ClientError from e
