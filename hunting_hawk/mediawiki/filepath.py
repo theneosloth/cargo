56c5f11c7f29931d021f6ffb373c8df950e1f6ed
Stefan Kuznetsov <neosloth@posteo.net>
2023-04-13 00:07:44 -0700

    Close resource

--- hunting_hawk/mediawiki/filepath.py
+++ hunting_hawk/mediawiki/filepath.py
@@ -6,7 +6,8 @@ ImageName = str
 def get_file_path(client: Client, file: ImageName) -> str:
     url = f"{client.index_endpoint()}/Special:FilePath/{file}"
     try:
-        res = raw_cached_get(client, url, {})
-    except ClientError:
-        return file
-    return res.url
+        with requests.get(url, follow_redirects=True, headers=client.headers, stream=True) as res:
+            url = res.url
+        return url
+    except requests.exception.HTTPError as e:
+        raise ClientError from e
