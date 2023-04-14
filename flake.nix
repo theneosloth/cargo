96261e0266198b7b0196c91cae5bda603756e734
Stefan Kuznetsov <neosloth@posteo.net>
2023-04-14 15:45:33 -0700

    Fix pyproject

--- flake.nix
+++ flake.nix
@@ -1,50 +1,4 @@
 {
-<<<<<<< HEAD
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }: {
-    overlay = nixpkgs.lib.composeManyExtensions [
-      (final: prev: {
-        myApp = prev.poetry2nix.mkPoetryApplication {
-          projectDir = self;
-        };
-      })
-    ];
-  } // (flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      };
-    in
-    {
-      apps = {
-        inherit (pkgs) myApp;
-      };
-
-      defaultApp = pkgs.myApp;
-
-      devShell =
-        let
-          pythonPackages = pkgs.python310Packages;
-        in
-        pkgs.mkShellNoCC {
-          buildInputs = with pythonPackages; [ python venvShellHook ];
-          packages = [ pkgs.poetry ];
-          venvDir = "./.venv";
-          postVenvCreation = ''
-            unset SOURCE_DATE_EPOCH
-            poetry env use .venv/bin/python
-            poetry install
-          '';
-          postShellHook = ''
-            unset SOURCE_DATE_EPOCH
-            poetry env info
-          '';
-        };
-    }));
-=======
     inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     outputs = { self, nixpkgs }:
@@ -99,5 +53,4 @@
                   };
               });
           };
->>>>>>> b02f42ddf13b2bb9971a410d0edfd4b28ad148f0
 }
