fbde2b33c5d1bf36ba57eacd1256b542f75d62f5
Stefan Kuznetsov <neosloth@posteo.net>
2023-04-30 21:27:27 -0700

    Don't fetch image

--- hunting_hawk/mediawiki/filepath.py
+++ hunting_hawk/mediawiki/filepath.py
@@ -7,14 +7,15 @@ ImageName = str
 def get_file_path(client: Client, file: ImageName) -> str:
     if not file:
         return ""
-    url = f"{client.index_endpoint()}/Special:FilePath/{file}"
-    try:
-        res = requests.get(
-            url, allow_redirects=True, headers=client.headers, stream=True
-        )
-        res.raise_for_status()
-        res.close()
-        url = res.url
-        return url
-    except requests.exceptions.HTTPError as e:
-        return file
+    return url = f"{client.index_endpoint()}/Special:FilePath/{file}"
+    # TODO: Cache can be populated with:
+    # try:
+    #     res = requests.get(
+    #         url, allow_redirects=True, headers=client.headers, stream=True
+    #     )
+    #     res.raise_for_status()
+    #     res.close()
+    #     url = res.url
+    #     return url
+    # except requests.exceptions.HTTPError as e:
+    #     return file
