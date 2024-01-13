7c6ea50af1bf7e463d154aac0952f70388078277
Stefan Kuznetsov <neosloth@sent.com>
2024-01-13 14:10:06 -0800

    Update pinned versions

--- flake.nix
+++ flake.nix
@@ -12,18 +12,16 @@
         flake-utils.lib.eachDefaultSystem (system:
             let
                 # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-                inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication defaultPoetryOverrides mkPoetryEnv;
+                inherit (import poetry2nix {inherit pkgs;}) mkPoetryApplication defaultPoetryOverrides mkPoetryEnv;
                 pkgs = nixpkgs.legacyPackages.${system};
                 overrides = defaultPoetryOverrides.extend
                     (self: super: {
                         ruff = super.ruff.override {
                             preferWheel = true;
                         };
-                        wikitextparser = super.wikitextparser.overridePythonAttrs(
-                            old: {
-                                buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
-                            }
-                        );
+                        mypy = super.mypy.override {
+                            preferWheel = true;
+                        };
                     });
             in
               {
